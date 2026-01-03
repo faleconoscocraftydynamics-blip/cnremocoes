02d23a0199d06f3f32aa0dca68ed658077729658
faleconoscocraftydynamics-blip <faleconosco.craftydynamics@gmail.com>
2026-01-03 20:23:08 -0300

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from datetime import date, time
 
-BACKEND_URL = "http://localhost:8000/submit"
+BACKEND_URL = "https://cnremocoes.onrender.com/submit"
 
 st.set_page_config(page_title="Formulário de Serviço Médico", layout="centered")
 
