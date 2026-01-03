26d494194d42d421fc7c9d81b0af7350e1ee794d
faleconoscocraftydynamics-blip <faleconosco.craftydynamics@gmail.com>
2026-01-03 20:03:01 -0300

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Response
+from pdf_generator import generate_pdf
+
+app = FastAPI()
+
+@app.post("/submit")
+async def submit(data: dict):
+  pdf_bytes = generate_pdf(data)
+
+return Response(
+  content=pdf_bytes, 
+  media_type="application/pdf",
+  headers={
+    "Content-Disposition":"attachment; filename=medical_service_report.pdf"
+  },
+)
